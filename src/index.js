c34b90364768f0e169b278c5cb0809095d1a2c83
Maheswar147 <55059791+mahesh12393@users.noreply.github.com>
2021-02-14 23:13:33 +0530

    First react app

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>Hello World!</h1>
+    <p>My very first React app. Super excited to learn more!</p>
+  </div>,
+  document.getElementById("root")
+);
